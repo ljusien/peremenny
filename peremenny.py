a88a0ff21ae86ac13cd0702bbcecb2430537fc60
ljusien <ljusien777@gmail.com>
2024-11-17 20:52:13 +0300

    Add files via upload

---
+++ peremenny.py
@@ -0,0 +1,12 @@
+name=('Курс: Python')
+print(name)
+first_name=('Всего задач:')
+a_count = 12
+second_name=('Затрачено часов:')
+b_count = 1.5
+third_name=('Среднее время выполнения')
+c_count = 1.5/12
+print(third_name)
+print(first_name, '12,', second_name, '1.5,', third_name, '0.125')
+
+
